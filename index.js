23c59624bd6ab414b95b38723d5098459f64d2fd
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2016-03-31 19:12:37 -0400

    finished quiz pages

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var questions = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+var questions = [1,2,3,4,5,6,7,8,9,10,11];
 var answers = ["A1","A2","A3","A4","A5"];
 
 $(document).ready(function(){
@@ -6,6 +6,7 @@ $(document).ready(function(){
   var mq = window.matchMedia("(max-width: 500px");
   if (mq.matches){
     console.log("mobile");
+    $("#desktop").addClass("hide");
     for (i=0; i<questions.length; i++){
       var qNum = questions[i];
       $("#question").append("<li class='questions'>"+qNum+"<ol id='"+qNum+"'></ol></li>");
@@ -15,23 +16,49 @@ $(document).ready(function(){
       }
     }
     $(".answers").click(selectAnswer);
-    $("ul").append("<button>Submit</button>");
+    $("ul").append("<button onclick=\"location.href='results.html'\">Submit</button>");
   }
   else{
-    var start = 0;
-    for (i=start; i<start+3; i++){
-      var qNum = questions[i];
-      $("#question").append("<li class='questions'>"+qNum+"<ol id='"+qNum+"'></ol></li><button class=hide>Submit</button>");
-      // var A = $("ol").children.getAttribute('id');
-      for (j=0; j<answers.length; j++){
-        $("#"+qNum).append("<li class='answers' id='"+qNum+answers[j]+"''>"+answers[j]+"</li>");
-      }
-    }
-    $(".answers").click(selectAnswer);
-    $("button").click(loadNext);
+    $('#question').addClass("hide");
+    $('#q2').addClass('hide');
+    $('#q3').addClass('hide');
+    $('#q4').addClass('hide');
 
+    // $('#b1').click(nextPage);
+    $("#b1").click(function(){
+      $("#q1").addClass('hide');
+      $('#q2').removeClass('hide');
+    });
+
+    $("#b2").click(function(){
+      $("#q2").addClass('hide');
+      $('#q3').removeClass('hide');
+    });
+
+    $("#b3").click(function(){
+      $("#q3").addClass('hide');
+      $('#q4').removeClass('hide');
+      $('#bl').removeClass("buttonHide");
+    });
+    
+    $(".answers").click(selectAnswer);
+    // var start = 0;
+    // for (i=start; i<start+3; i++){
+    //   var qNum = questions[i];
+    //   $("#question").append("<li class='questions'>"+qNum+"<ol id='"+qNum+"'></ol></li><button class=hide>Submit</button>");
+    //   // var A = $("ol").children.getAttribute('id');
+    //   for (j=0; j<answers.length; j++){
+    //     $("#"+qNum).append("<li class='answers' id='"+qNum+answers[j]+"''>"+answers[j]+"</li>");
+    //   }
+    // }
+    // $(".answers").click(selectAnswer);
+    // $("button").click(loadNext);
   }
+  
 });
+
+
+
 function selectAnswer(){
   var answer = this.id;
   console.log(answer);
@@ -43,23 +70,4 @@ function selectAnswer(){
     $(this).addClass("selected");
     console.log("Chosen");
   }
-}
-
-function loadNext(){
-  // $(".questions").addClass("collapse");
-  // $('button').addClass("collapse");
-
-  console.log("initial: "+i);
-  var start = i;
-  for (i=start; i<start+3; i++){
-    console.log(i);
-    var qNum = questions[i];
-      $("#question").append("<li class='questions'>"+qNum+"<ol id='"+qNum+"'></ol></li><button class=hide>Submit</button>");
-      // var A = $("ol").children.getAttribute('id');
-      for (j=0; j<answers.length; j++){
-        $("#"+qNum).append("<li class='answers' id='"+qNum+answers[j]+"''>"+answers[j]+"</li>");
-      }
-    }
-    $(".answers").click(selectAnswer);
-    console.log("final: " + i);
 }
