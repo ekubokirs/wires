1b61c4d25b133843b274cbb9f289805fcd70df69
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2016-04-07 19:26:06 -0400

    added mobile results

--- js/results.js
+++ js/results.js
@@ -3,7 +3,7 @@ var breed = ["Compare", 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 $(document).ready(function(){
   for (i=0; i<criteria.length; i++){
     var c = criteria[i];
-    $("ul").append("<li>Criteria " + c + "</li>");
+    $(".breedElements").append("<li class='criteria'>Criteria " + c + "</li>");
   }
   for (j=0; j<breed.length; j++){
     var breeds = breed[j];
